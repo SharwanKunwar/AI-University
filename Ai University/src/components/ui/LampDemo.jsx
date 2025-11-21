81d29c2260a8d434b297cb8ca906df7bc0f161d0
Sharwan jung kunwar <sharwankunwar07@gmail.com>
2025-11-21 18:55:33 +0545

    updted

--- Ai University/src/components/ui/LampDemo.jsx
+++ Ai University/src/components/ui/LampDemo.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 // eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
-import { LampContainer } from "./lamp";
+import { LampContainer } from "../ui/LampContainer";
 import { FaFacebook, FaGithub, FaLinkedin } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
